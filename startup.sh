5f18b10d78c4f48627f242025c9a871b6b8c3bb4
Bharat Naik <bharatnaik777@gmail.com>
2022-08-29 17:31:09 +0530

    Added startup of ssh keys for git

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# For starting up imwheel service
 imwheel
+
+# For adding up the ssh keys for git
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_ed25519_github
+ssh-add ~/.ssh/id_ed25519_gitlab
